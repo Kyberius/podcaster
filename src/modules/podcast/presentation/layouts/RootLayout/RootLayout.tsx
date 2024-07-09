2bf11ea058a8c7fb060c430a1a0e98e6c5514fac
Alex Valencia Gallego <alexvlc@duck.com>
2024-07-09 16:53:12 +0200

    feat: adaptable tablet

--- src/modules/podcast/presentation/layouts/RootLayout/RootLayout.tsx
+++ src/modules/podcast/presentation/layouts/RootLayout/RootLayout.tsx
@@ -1,7 +1,7 @@
 import Header from 'modules/podcast/presentation/components/Header'
 import { Outlet, useNavigation } from 'react-router-dom'
 import styles from './RootLayout.module.scss'
-import { useIsFetching } from '@tanstack/react-query'
+
 const RootLayout = () => {
   const { state } = useNavigation()
   return (
